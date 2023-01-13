02831a97f78d082b71d6145b1b8dcb95163d0bbd
Cristi Gard <65028018+cristigard@users.noreply.github.com>
2023-01-14 00:34:21 +0200

    Create add_column_to_dbf_existing_file.py

---
+++ add_column_to_dbf_existing_file.py
@@ -0,0 +1,24 @@
+import dbf
+
+#open file
+table = dbf.Table(str('C:/Users/USER IT-04/PycharmProjects/pythonProject/temptable.dbf'), codepage='ascii')
+
+#open table
+table.open(mode=dbf.READ_WRITE)
+
+#add new column to table
+table.add_fields('TELEPHONE C(10)')
+
+#add data to new format table
+table.append({'NR_NIR': '1', 'NR_INTRARE': '111', 'GESTIUNE': 'gest', 'DEN_GEST': 'den gest', 'COD': '222',
+              'DATA':dbf.Date(1979, 9,13), 'SCADENT': dbf.Date(1979, 9,13), 'TIP': 'A', 'TVAI': 1, 'COD_ART': 'cod articol',
+              'DEN_ART': 'den articol', 'UM': 'buc', 'CANTITATE': 12, 'DEN_TIP': 'den tip', 'TVA_ART': 19,
+              'VALOARE': 20.33, 'TVA': 25, 'CONT': 'cont', 'PRET_VANZ': 220, 'GRUPA': 'grupa', 'TELEPHONE': 123456789
+              })
+
+table.close()
+
+
+
+
+
