682ac731973436eba787fb9623480d9065829d57
Cristi Gard <65028018+cristigard@users.noreply.github.com>
2023-01-14 00:53:07 +0200

    Update add_column_to_dbf_existing_file.py

--- add_column_to_dbf_existing_file.py
+++ add_column_to_dbf_existing_file.py
@@ -13,7 +13,7 @@ table.add_fields('TELEPHONE C(10)')
 table.append({'NR_NIR': '1', 'NR_INTRARE': '111', 'GESTIUNE': 'gest', 'DEN_GEST': 'den gest', 'COD': '222',
               'DATA':dbf.Date(1979, 9,13), 'SCADENT': dbf.Date(1979, 9,13), 'TIP': 'A', 'TVAI': 1, 'COD_ART': 'cod articol',
               'DEN_ART': 'den articol', 'UM': 'buc', 'CANTITATE': 12, 'DEN_TIP': 'den tip', 'TVA_ART': 19,
-              'VALOARE': 20.33, 'TVA': 25, 'CONT': 'cont', 'PRET_VANZ': 220, 'GRUPA': 'grupa', 'TELEPHONE': 123456789
+              'VALOARE': 20.33, 'TVA': 25, 'CONT': 'cont', 'PRET_VANZ': 220, 'GRUPA': 'grupa', 'TELEPHONE': '123456789'
               })
 
 table.close()
